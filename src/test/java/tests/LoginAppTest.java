fea494ee1ee13a11f9c567e406524ae88600d0e0
Alexander Perez Zapata <alexander.pz@globant.com>
2021-02-11 11:14:02 -0500

    initial commit

---
+++ src/test/java/tests/LoginAppTest.java
@@ -0,0 +1,26 @@
+package tests;
+
+import org.springframework.context.annotation.Description;
+import org.testng.annotations.Test;
+
+import screens.DashBoardScreen;
+import screens.LoginScreen;
+import util.tests.BaseMobileTest;
+
+public class LoginAppTest extends BaseMobileTest {
+
+//	/**
+//	 * Verify something.
+//	 */
+	@Description(value = "test of something ID:0001")
+	@Test()
+	public void testOfSomething() {
+		log.info("testOfSomething");
+		DashBoardScreen dashBoard = loadDashBoardScreen();
+		LoginScreen login = dashBoard.goToLoginScreen();
+		login.fillInLoginData("email", "password");	
+	}
+
+	
+
+}
